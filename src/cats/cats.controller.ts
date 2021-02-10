ba04c565ea43d089105ba8baa6ba9c0556906ea8
ChangHyunKim <hot9998@naver.com>
2021-02-10 16:57:10 +0900

    Add cats controller CRUD apis

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CatDto } from './cat.dto';
 
 @Controller('cats')
 export class CatsController {
-  @Get()
+  @Post()
+  create(@Body() catDto: CatDto): string {
+    console.log(catDto);
+    return 'This action adds a new cat';
+  }
+
+  @Get('')
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() catDto: CatDto) {
+    return `This action updates a #${id} cat`;
   }
 }
