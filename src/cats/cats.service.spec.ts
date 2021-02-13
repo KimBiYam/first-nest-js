df26f4cbf57255b7350f7c1bcfab1258f64b3990
ChangHyunKim <hot9998@naver.com>
2021-02-13 19:00:17 +0900

    Add cats service tests

--- src/cats/cats.service.spec.ts
+++ src/cats/cats.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from './cats.service';
 
@@ -14,5 +15,38 @@ describe('CatsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll()', () => {
+    it('should return an array', () => {
+      const result = service.findAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('findOne()', () => {
+    it('should find a cat', () => {
+      service.create({
+        age: 12,
+        breed: 'Test',
+        favoriteThings: ['test'],
+        name: 'test',
+      });
+
+      const cat = service.findOne(1);
+      expect(cat).toBeDefined();
+      expect(cat.id).toEqual(1);
+      expect(cat.name).toEqual('test');
+    });
+
+    it('should throw not found exception', () => {
+      const id = 999;
+      try {
+        service.findOne(id);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual(`Cat with Id ${id} not found.`);
+      }
+    });
   });
 });
